e29976194937794a2b2046c4ccb239f8496a80eb
Alex Haug <stahh02@moravian.edu>
2019-08-23 17:20:00 -0400

    Adding .gitignore and setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,12 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
+    name='World_1-1_Recreation',
+    version='0.0.0',
+    description='',
+    long_description='Read README.md',
+    author='Alex Haug',
+    url='https://github.com/AlexanderHaug/World_1-1_Recreation',
     python_requires='>=3.7.2',
-    install_requires=['pygame'],
+    install_requires=['pygame >=1.9.6'],
 )
